14877c8ccb36553789dd725a83b86145e8711eb8
StubbornVegeta <vegetaking@126.com>
2024-03-09 17:17:48 +0800

    update keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,4 +6,4 @@ local keymap = vim.keymap
 keymap.set("n", "S", "<cmd>w<cr>")
 keymap.set("n", "Q", "<cmd>quit<cr>")
 keymap.set("n", "<leader>d", "<cmd>Dashboard<cr>")
-keymap.set("n", "<leader>rc", "<cmd>e ~/.config/nvim/lua/<cr>")
+-- <leader>fc open nvim config
