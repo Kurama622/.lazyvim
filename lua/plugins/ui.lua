34c9faf5c446a9073effd4e6807124408d335e65
vegeta <vegetaking@126.com>
2024-07-31 22:37:29 +0800

    update lualine style

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,3 +1,13 @@
+local treesitter = require("nvim-treesitter")
+local function treelocation()
+  return treesitter.statusline({
+    indicator_size = 100,
+    -- type_patterns = { "class", "function", "method", "for", "while", "if", "switch", "case" },
+    type_patterns = { "class", "function", "method" },
+    separator = " ▸ ",
+  })
+end
+
 return {
   {
     "nvim-lualine/lualine.nvim",
@@ -7,6 +17,26 @@ return {
           theme = "jellybeans",
           section_separators = { left = "", right = "" },
           component_separators = { left = "", right = "" },
+        },
+        sections = {
+          lualine_c = {
+            "filename",
+            { treelocation, color = LazyVim.ui.fg("Type") },
+          },
+          lualine_x = {
+            {
+              function()
+                return require("noice").api.status.command.get()
+              end,
+              cond = function()
+                return package.loaded["noice"] and require("noice").api.status.command.has()
+              end,
+              color = LazyVim.ui.fg("Statement"),
+            },
+            "encoding",
+            "fileformat",
+            "filetype",
+          },
         },
       })
     end,
