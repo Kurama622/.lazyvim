0bc300557b44d66d0ffd20f4ce712215546a6d62
Kurama <kurama174@163.com>
2024-09-04 21:53:06 +0800

    update colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  -- catppuccin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+    },
+  },
+  -- nvim colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
