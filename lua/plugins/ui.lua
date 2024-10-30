d611b40c080e29cae061e3558aa84432dac223cb
Kurama <kurama174@163.com>
2024-10-30 23:42:32 +0800

    update profile settings

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -28,6 +28,9 @@ return {
   },
   {
     "3rd/image.nvim",
+    dependencies = {
+      "leafo/magick",
+    },
     config = function()
       require("image").setup({
         backend = "ueberzug",
@@ -36,7 +39,7 @@ return {
         integrations = {
           markdown = {
             enabled = true,
-            clear_in_insert_mode = false,
+            clear_in_insert_mode = true,
             download_remote_images = true,
             only_render_image_at_cursor = false,
             filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
@@ -46,9 +49,9 @@ return {
         max_height = nil,
         max_width_window_percentage = nil,
         max_height_window_percentage = 50,
-        window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+        window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
         -- window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-        editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+        editor_only_render_when_focused = true, -- auto show/hide images when the editor gains/looses focus
         tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
         hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
       })
@@ -60,7 +63,17 @@ return {
     name = "profile",
     dev = { true },
     config = function()
-      require("profile")
+      require("profile").setup({
+        avatar_opts = {
+          avatar_width = 20,
+          avatar_height = 20,
+          avatar_x = (vim.o.columns - 20) / 2,
+          avatar_y = 4,
+        },
+        config = {
+          avatar = true,
+        },
+      })
     end,
   },
 }
