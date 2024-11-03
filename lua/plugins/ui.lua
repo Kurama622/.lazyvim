aac04a513e6d7ca8549f8964bd5619e3c9cf9f7f
Kurama <kurama174@163.com>
2024-11-03 15:26:47 +0800

    add profile.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -41,8 +41,14 @@ return {
             enabled = true,
             clear_in_insert_mode = true,
             download_remote_images = true,
-            only_render_image_at_cursor = false,
+            only_render_image_at_cursor = true,
             filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+          },
+          html = {
+            enabled = false,
+          },
+          css = {
+            enabled = false,
           },
         },
         max_width = nil,
@@ -57,23 +63,75 @@ return {
       })
     end,
   },
+
   {
-    dir = "/home/arch/Github/profile.nvim",
+    "Kurama622/profile.nvim",
+    -- dir = "/home/arch/Github/profile.nvim",
     dependencies = { "3rd/image.nvim" },
-    name = "profile",
-    dev = { true },
     config = function()
+      local comp = require("profile.components")
       require("profile").setup({
+        avatar_path = "/home/arch/Github/profile.nvim/resources/profile.png",
         avatar_opts = {
           avatar_width = 20,
           avatar_height = 20,
           avatar_x = (vim.o.columns - 20) / 2,
-          avatar_y = 4,
+          avatar_y = 7,
         },
-        config = {
-          avatar = true,
+        user = "Kurama622",
+        git_contributions = {
+          start_week = 1,
+          end_week = 53,
+          empty_char = " ",
+          full_char = { "", "󰧞", "", "", "" },
+          fake_contributions = nil,
+          -- fake_contributions = function()
+          --   local ret = {}
+          --   for i = 1, 53 do
+          --     ret[tostring(i)] = {}
+          --     for j = 1, 7 do
+          --       ret[tostring(i)][j] = math.random(0, 5)
+          --     end
+          --   end
+          --   return ret
+          -- end,
         },
+        hide = {
+          statusline = true,
+          tabline = true,
+        },
+        format = function()
+          comp:avatar()
+          comp:text_component_render({
+            comp:text_component("git@github.com:Kurama622/profile.nvim", "center", "ProfileRed"),
+            comp:text_component("──── By Kurama622", "right", "ProfileBlue"),
+          })
+          comp:seperator_render()
+          comp:card_component_render({
+            type = "table",
+            content = function()
+              return {
+                {
+                  title = "kurama622/llm.nvim",
+                  description = [[LLM Neovim Plugin: Effortless Natural
+           Language Generation with LLM's API]],
+                },
+                {
+                  title = "kurama622/profile.nvim",
+                  description = [[A Neovim plugin: Your Personal Homepage]],
+                },
+              }
+            end,
+            hl = {
+              border = "ProfileYellow",
+              text = "ProfileYellow",
+            },
+          })
+          comp:seperator_render()
+          comp:git_contributions_render("ProfileGreen")
+        end,
       })
+      vim.api.nvim_set_keymap("n", "<leader>p", "<cmd>Profile<cr>", { silent = true })
     end,
   },
 }
