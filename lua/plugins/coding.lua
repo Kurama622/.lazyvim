0eb7763b1e98b5b5d688376d6c2b69c43abdec8d
Kurama <kurama174@163.com>
2024-11-23 23:10:41 +0800

    add markdown-toc

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -193,6 +193,20 @@ return {
       { "<leader>ml", "<cmd>call org#main#runLanguage()<cr>", desc = "run language" },
     },
   },
+  {
+    "hedyhli/markdown-toc.nvim",
+    ft = "markdown", -- Lazy load on markdown filetype
+    cmd = { "Mtoc" }, -- Or, lazy load on "Mtoc" command
+    -- opts = {
+    --   -- Your configuration here (optional)
+    -- },
+    config = function()
+      require("mtoc").setup({})
+    end,
+    keys = {
+      { "<leader>mt", "<cmd>Mtoc<cr>", desc = "gen markdown toc" },
+    },
+  },
 
   -- git
   {
