39b8f91b16fbc42396a717121e4a68a4bff8531b
Kurama <kurama174@163.com>
2024-12-22 21:43:15 +0800

    add render-markdown

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -207,6 +207,15 @@ return {
       { "<leader>mt", "<cmd>Mtoc<cr>", desc = "gen markdown toc" },
     },
   },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
 
   -- git
   {
