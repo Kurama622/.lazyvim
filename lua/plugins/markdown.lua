ff41b95cc2320b903753bbe6640b02bd8a6b605d
Kurama <kurama174@163.com>
2025-01-01 19:37:07 +0800

    update render_markdown hl

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -49,22 +49,67 @@ return {
   },
   {
     "Kurama622/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {
-      dash = {
-        -- Turn on / off thematic break rendering
-        enabled = true,
-        -- Replaces '---'|'***'|'___'|'* * *' of 'thematic_break'
-        icon = "─",
-        width = 0.5,
-        left_margin = 0.5,
-        -- Highlight for the whole line generated from the icon
-        highlight = "RenderMarkdownDash",
-      },
-    },
+
+    config = function()
+      vim.api.nvim_set_hl(0, "@markup.raw.markdown_inline", { fg = "#4fd6be", bg = "NONE" })
+      require("render-markdown").setup({
+        heading = {
+          enabled = true,
+          sign = false,
+          position = "inline",
+          icons = { "██ ", "█ ", "█ ", "▌ ", "▌ ", "│ " },
+          signs = { "󰫎 " },
+          width = "block",
+          left_margin = 0,
+          left_pad = 0,
+          right_pad = 0,
+          min_width = 0,
+          border = false,
+          border_virtual = false,
+          border_prefix = false,
+          above = "▄",
+          below = "▀",
+          backgrounds = {},
+          foregrounds = {
+            "RenderMarkdownH1",
+            "RenderMarkdownH2",
+            "RenderMarkdownH3",
+            "RenderMarkdownH4",
+            "RenderMarkdownH5",
+            "RenderMarkdownH6",
+          },
+        },
+        dash = {
+          enabled = true,
+          icon = "─",
+          width = 0.5,
+          left_margin = 0.5,
+          highlight = "RenderMarkdownDash",
+        },
+        code = {
+          enabled = true,
+          sign = true,
+          style = "none",
+          position = "left",
+          language_pad = 0,
+          language_name = true,
+          disable_background = { "diff" },
+          width = "full",
+          left_margin = 0,
+          left_pad = 0,
+          right_pad = 0,
+          min_width = 0,
+          border = "thin",
+          above = "▄",
+          below = "▀",
+          highlight = "RenderMarkdownCode",
+          highlight_inline = "RenderMarkdownCodeInline",
+          highlight_language = nil,
+        },
+      })
+    end,
   },
 }
