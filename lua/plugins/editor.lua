a7acba866068d7410abca230ac45c0e954797955
Kurama <kurama174@163.com>
2025-01-01 19:37:21 +0800

    add fzf-lua and blink.cmp

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,3 @@
-local Util = require("lazyvim.util")
 return {
   -- add symbols-outline
   {
@@ -7,54 +6,41 @@ return {
     keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
     config = true,
   },
-
   {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    },
-    opts = function()
-      local actions = require("telescope.actions")
-
-      local find_files_no_ignore = function()
-        local action_state = require("telescope.actions.state")
-        local line = action_state.get_current_line()
-        Util.telescope("find_files", { no_ignore = true, default_text = line })()
-      end
-      local find_files_with_hidden = function()
-        local action_state = require("telescope.actions.state")
-        local line = action_state.get_current_line()
-        Util.telescope("find_files", { hidden = true, default_text = line })()
-      end
-
-      return {
-        defaults = {
-          layout_config = {
-            horizontal = {
-              preview_width = 0.5, -- make this smaller works
-            },
-          },
-          mappings = {
-            i = {
-              ["<a-i>"] = find_files_no_ignore,
-              ["<a-h>"] = find_files_with_hidden,
-              ["<c-n>"] = actions.cycle_history_next,
-              ["<c-p>"] = actions.cycle_history_prev,
-              ["<c-j>"] = actions.move_selection_next,
-              ["<c-k>"] = actions.move_selection_previous,
-              ["<C-f>"] = actions.preview_scrolling_down,
-              ["<C-b>"] = actions.preview_scrolling_up,
-            },
-          },
+    "ibhagwan/fzf-lua",
+    config = function()
+      require("fzf-lua").setup({
+        fzf_colors = {
+          false, -- inherit fzf colors that aren't specified below from
+          -- the auto-generated theme similar to `fzf_colors=true`
+          ["fg"] = { "fg", "CursorLine" },
+          ["bg"] = { "bg", "Normal" },
+          ["hl"] = { "fg", "Comment" },
+          ["hl+"] = { "fg", "Statement" },
+          ["info"] = { "fg", "PreProc" },
+          ["prompt"] = { "fg", "Conditional" },
+          ["marker"] = { "bg", "Keyword" },
+          ["spinner"] = { "fg", "Label" },
+          ["pointer"] = { "fg", "Exception" },
+          ["header"] = { "fg", "Comment" },
+          ["gutter"] = "-1",
         },
-      }
+      })
     end,
   },
-
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        menu = {
+          border = "rounded",
+          winhighlight = "Normal:BlinkCmpMenu,FloatBorder:FloatBorder",
+        },
+        documentation = { window = { border = "rounded" } },
+      },
+      signature = { window = { border = "single" } },
+    },
+  },
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
