63161eb1bba85e4b7f9ef90085a265588a836be1
Kurama622 <kurama174@163.com>
2025-02-15 13:10:34 +0800

    update ft

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -52,6 +52,7 @@ return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+    ft = { "markdown", "llm" },
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
 
