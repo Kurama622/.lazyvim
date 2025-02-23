865fd05c796d9846351430f2c0ce26ddb449c7d0
Kurama622 <kurama174@163.com>
2025-02-23 21:25:12 +0800

    update llm tools' zindex

--- lua/plugins/llm/app.lua
+++ lua/plugins/llm/app.lua
@@ -106,6 +106,9 @@ return {
         url = "https://open.bigmodel.cn/api/paas/v4/chat/completions",
         model = "glm-4-flash",
         api_type = "zhipu",
+        win_opts = {
+          zindex = 120,
+        },
         -- args = [=[return string.format([[curl %s -N -X POST -H "Content-Type: application/json" -H "Authorization: Bearer %s" -d '%s']], url, LLM_KEY, vim.fn.json_encode(body))]=],
         -- args = [[return {url, "-N", "-X", "POST", "-H", "Content-Type: application/json", "-H", authorization, "-d", vim.fn.json_encode(body)}]],
         exit_on_move = true,
@@ -141,6 +144,7 @@ return {
         win_opts = {
           relative = "editor",
           position = "50%",
+          zindex = 100,
         },
         accept = {
           mapping = {
