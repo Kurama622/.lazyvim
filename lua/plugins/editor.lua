3c408dcaec66ed96e586a73366dcb94e4ad593d9
Kurama622 <kurama174@163.com>
2025-11-01 13:46:59 +0800

    feat: update key mappings for FzfLua integration
    - Replace old picker functions with FzfLua commands

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -25,77 +25,12 @@ return {
       explorer = { enabled = false },
     },
     keys = {
-      -- Top Pickers & Explorer
-      {
-        "<leader><space>",
-        function()
-          Snacks.picker.smart()
-        end,
-        desc = "Smart Find Files",
-      },
-      {
-        "<leader>,",
-        function()
-          Snacks.picker.buffers()
-        end,
-        desc = "Buffers",
-      },
-      {
-        "<leader>/",
-        function()
-          Snacks.picker.grep()
-        end,
-        desc = "Grep",
-      },
-      {
-        "<leader>:",
-        function()
-          Snacks.picker.command_history()
-        end,
-        desc = "Command History",
-      },
       {
         "<leader>n",
         function()
           Snacks.picker.notifications()
         end,
         desc = "Notification History",
-      },
-      -- find
-      {
-        "<leader>fb",
-        function()
-          Snacks.picker.buffers()
-        end,
-        desc = "Buffers",
-      },
-      {
-        "<leader>fc",
-        function()
-          Snacks.picker.files({ cwd = fn.stdpath("config") })
-        end,
-        desc = "Find Config File",
-      },
-      {
-        "<leader>ff",
-        function()
-          Snacks.picker.files()
-        end,
-        desc = "Find Files",
-      },
-      {
-        "<leader>fp",
-        function()
-          Snacks.picker.projects()
-        end,
-        desc = "Projects",
-      },
-      {
-        "<leader>fr",
-        function()
-          Snacks.picker.recent()
-        end,
-        desc = "Recent",
       },
       -- git
       {
@@ -672,6 +607,43 @@ return {
         },
       })
     end,
+    keys = {
+      {
+        "<leader><space>",
+        "<cmd>FzfLua files<CR>",
+        desc = "Find Files",
+      },
+      {
+        "<leader>ff",
+        "<cmd>FzfLua files<CR>",
+        desc = "Find Files",
+      },
+      {
+        "<leader>fc",
+        "<cmd>FzfLua files cwd=$HOME/.config/nvim<CR>",
+        desc = "Open Configs",
+      },
+      {
+        "<leader>/",
+        "<cmd>FzfLua live_grep<cr>",
+        desc = "Live Grep",
+      },
+      {
+        "<leader>fr",
+        "<cmd>FzfLua oldfiles<cr>",
+        desc = "Open Recent Files",
+      },
+      {
+        "<leader>fb",
+        "<cmd>FzfLua buffers<cr>",
+        desc = "Open Buffers",
+      },
+      {
+        "<leader>:",
+        "<cmd>FzfLua command_history<cr>",
+        desc = "Find Command History",
+      },
+    },
   },
 
   -- {
