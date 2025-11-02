70ae469cf809f70f71164c296f4777d909b6dcc2
Kurama622 <kurama174@163.com>
2025-11-02 21:35:33 +0800

    feat: add autocmd to restore cursor position on buffer read
    - Create BufReadPost autocmd
    
    - Restore cursor position if valid

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -15,3 +15,13 @@ api.nvim_create_user_command("Lg", function(opts)
 end, {
   nargs = "*",
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local last_pos = vim.fn.line([['"]])
+    if last_pos > 0 and last_pos <= vim.fn.line("$") then
+      vim.cmd([[normal! g`"]])
+    end
+  end,
+})
