6e7224417a81913a32d82baa046e6487d7715b2d
Kurama622 <kurama174@163.com>
2025-11-15 20:14:17 +0800

    feat: enhance Pyright settings
    - Enable single file support
    
    - Set type checking mode to basic

--- lsp/basedpyright.lua
+++ lsp/basedpyright.lua
@@ -33,12 +33,14 @@ return {
     "pyrightconfig.json",
     ".git",
   },
+  single_file_support = true,
   settings = {
     basedpyright = {
       analysis = {
         autoSearchPaths = true,
         useLibraryCodeForTypes = true,
         diagnosticMode = "openFilesOnly",
+        typeCheckingMode = "basic",
       },
     },
   },
