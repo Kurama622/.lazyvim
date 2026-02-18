5c5e3a5375356eca46d640f633b32894d377caec
Kurama622 <kurama174@163.com>
2026-02-18 23:04:39 +0800

    fix: escape percentage signs in commit message
    - Add escaping for '%' in commit messages
    
    - Ensure proper handling of special characters

--- lua/plugins/llm/extensions/commit_msg.lua
+++ lua/plugins/llm/extensions/commit_msg.lua
@@ -20,12 +20,13 @@ return {
         mode = "n",
         keys = "<cr>",
       },
-      action = function(self, opts)
+      action = function()
         local contents = vim.api.nvim_buf_get_lines(0, 0, -1, true)
 
         local cmd = string.format('!git commit -m "%s"', table.concat(contents, '" -m "'))
         cmd = (cmd:gsub(".", {
           ["#"] = "\\#",
+          ["%"] = "\\%",
         }))
         vim.api.nvim_command(cmd)
 
