5133224bf6c416e27a30be100555fdf7275d871c
Kurama622 <kurama174@163.com>
2026-05-02 23:23:20 +0800

    feat: add GitHub dashboard plugin configuration
    - Include Kurama622/dashboard-github.nvim
    - Add custom layout and shortcuts
    - Set highlights for dashboard elements

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -563,4 +563,37 @@ return {
       },
     },
   },
+  {
+    "Kurama622/dashboard-github.nvim",
+    dependencies = { "ibhagwan/fzf-lua" },
+    opts = {
+      username = "Kurama622",
+      months_to_show = 8,
+
+      layout = {
+        gap = 8,
+        date_gap = 5,
+        plugin_info_gap = 5,
+        shortcuts_top_offset = 1,
+        plugin_info_offset = 1,
+      },
+
+      shortcuts = {
+        { key = "f", desc = "Find Files", action = "<cmd>FzfLua files<CR>" },
+        { key = "r", desc = "Recent Files", action = "<cmd>FzfLua oldfiles<CR>" },
+        { key = "n", desc = "New File", action = "<cmd>enew <CR>i" },
+        { key = "c", desc = "Open Neovim Config", action = "<cmd>FzfLua files cwd=~/.config/nvim<CR>" },
+        { key = "/", desc = "Live Grep", action = "<cmd>FzfLua live_grep<CR>" },
+        { key = "u", desc = "Update Plugins", action = "<cmd>Lazy update<CR>" },
+        { key = "q", desc = "Quit", action = "<cmd>qa<CR>" },
+      },
+      highlights = {
+        art = "DashboardArt",
+        key = "Operator",
+        desc = "DashboardKey",
+        date = "DashboardDate",
+        footer = "DashboardFooter",
+      },
+    },
+  },
 }
