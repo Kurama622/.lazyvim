b751f47e4eeb88573804e45991d19ca6046edd6d
Kurama622 <kurama174@163.com>
2026-05-05 22:21:05 +0800

    chore: update editor and modeline plugin configurations
    - Disable quickfile in editor plugin
    - Change Lazy UI keybinding to comment
    - Change modeline event to trigger on BufNewFile

--- lua/plugins/modeline.lua
+++ lua/plugins/modeline.lua
@@ -1,9 +1,8 @@
 return {
   {
     "Kurama622/modeline.nvim",
-    event = { "BufReadPost */*", "BufEnter" },
+    event = { "BufReadPost", "BufNewFile" },
     config = function()
-      vim.api.nvim_set_hl(0, "Statusline", { fg = "skyblue", bg = "NONE" })
       local p = require("modeline.provider")
       require("modeline").setup({
         p.mode(),
